42a61ea8bb1dc8b07f6a1f0c8bf8d7aaa6ed2fbf
HOPOLOK <vadim.truhanov@inbox.lv>
2024-11-26 19:20:43 +0200

    Knights finished with TLE

--- knights.cpp
+++ knights.cpp
@@ -3,7 +3,7 @@
 #include <queue>
 #include <unordered_map>
 #include <unordered_set>
-#include <cassert> //DEBUG
+// #include <cassert> //DEBUG
 #define forever while (1)
 using namespace std;
 
@@ -19,6 +19,51 @@ struct visit {
     int current;
     int dist; 
 };
+
+
+int slow_method(vector<unordered_map<int, edge*>> &edges, const int &nodes_amount, const int &start_node, const int &end_node) {
+    // DEBUG-start
+    // Reset flow in graph
+    for (const unordered_map<int, edge*> &m : edges) {
+        for (pair<int, edge*> p : m) {
+            p.second->flow = 0;
+        }
+    }
+    //DEBUG-end
+    int max_flow = 0;
+    forever {
+        unordered_set<int> visited;
+        queue<pair<int, int>> visit_queue;
+        visit_queue.push(pair<int, int>{start_node, start_node});
+        vector<int> parents(nodes_amount, -1);
+        while (!visit_queue.empty()) {
+            pair<int, int> visit_pair = visit_queue.front();
+            visit_queue.pop();
+            int cur_node = visit_pair.first, par_node = visit_pair.second;
+            if (visited.count(cur_node)) continue;
+            visited.insert(cur_node);
+            parents[cur_node] = par_node;
+            if (cur_node == end_node) break;
+            for (const pair<int, edge*> &p : edges[cur_node]) {
+                int next_node = p.first;
+                if (visited.count(next_node)) continue;
+                edge *cur_edge = p.second;
+                int cur_flow = cur_edge->flow;
+                if (cur_edge->goal == cur_node) cur_flow = cur_flow == 1 ? 0 : 1;
+                if (cur_flow == 1) continue;
+                visit_queue.push(pair<int, int>{next_node, cur_node}); 
+            }
+        }
+        if (parents[end_node] == -1) break;
+        max_flow++;
+        for (int cur_node = end_node, par_node = parents[cur_node]; cur_node != start_node; cur_node = par_node, par_node = parents[par_node]) {
+            edge *cur_edge = edges[cur_node][par_node];
+            int cur_flow = cur_edge->flow == 1 ? 0 : 1;
+            cur_edge->flow = cur_flow;
+        }
+    }
+    return max_flow;
+}
 
 
 vector<int> get_possible_pos(int p, int n) {
@@ -41,6 +86,84 @@ vector<int> get_possible_pos(int p, int n) {
         if (x + 1 < n)  pos.push_back((y+2) + (x+1)*n); 
     }
     return pos;
+}
+
+
+int fast_method(vector<unordered_map<int, edge*>> &edges, const int &nodes_amount, const int &start_node, const int &end_node) {
+    int max_flow = 0;
+    forever {
+        int end_dist = -1; 
+        {   //BFS
+            unordered_set<int> visited;
+            queue<pair<int, int>> visit_queue;
+            visit_queue.push(pair<int, int>{start_node, 0}); 
+            while (!visit_queue.empty()) {
+                pair<int, int> pp = visit_queue.front();
+                visit_queue.pop();
+                int cur_node = pp.first, cur_dist = pp.second;
+                if (visited.count(cur_node)) continue;
+                visited.insert(cur_node);
+                if (cur_node == end_node) {
+                    end_dist = cur_dist;
+                    break;
+                }
+                cur_dist++;
+                for (const pair<int, edge *> &pp : edges[cur_node]) {
+                    int next_node = pp.first;
+                    if (visited.count(next_node)) continue;
+                    edge *cur_edge = pp.second;
+                    int cur_flow = cur_edge->flow;
+                    if (cur_edge->goal == cur_node) cur_flow = cur_flow == 1 ? 0 : 1;
+                    if (cur_flow == 1) continue;
+                    visit_queue.push(pair<int, int>{next_node, cur_dist});
+                }
+            }
+        }
+        // int old_max_flow = max_flow; //DEBUG
+        if (end_dist == -1) break;
+        {   //DFS
+            unordered_map<int, int> visited_dist;
+            vector<visit> visit_stack;
+            visit_stack.push_back(visit{start_node, start_node, 0});
+            vector<int> parents(nodes_amount, -1);
+            while (!visit_stack.empty()) {
+                visit vv = visit_stack.back();
+                visit_stack.pop_back();
+                int 
+                cur_node = vv.current, 
+                par_node = vv.parent,
+                cur_dist = vv.dist;
+                if (visited_dist.count(cur_node)) if (visited_dist[cur_node] <= cur_dist) continue;
+                parents[cur_node] = par_node;
+                visited_dist[cur_node] = cur_dist;
+                if (cur_node == end_node) {
+                    for (; cur_node != start_node; cur_node = par_node, par_node = parents[par_node]) {
+                        edge *cur_edge = edges[cur_node][par_node];
+                        int cur_flow = cur_edge->flow == 1 ? 0 : 1;
+                        cur_edge->flow = cur_flow;
+                    }
+                    max_flow++;
+                    visited_dist.clear();
+                    visit_stack.clear();
+                    visit_stack.push_back(visit{start_node, start_node, 0});
+                    continue;
+                } else if (cur_dist >= end_dist) continue;
+                cur_dist++;
+                for (const pair<int, edge*> &pp : edges[cur_node]) {
+                    int next_node = pp.first;
+                    if (cur_dist >= end_dist) if (next_node != end_node) continue;
+                    else if (visited_dist.count(next_node)) if (visited_dist[next_node] <= cur_dist) continue;
+                    edge *cur_edge = pp.second;
+                    int cur_flow = cur_edge->flow;
+                    if (cur_edge->goal == cur_node) cur_flow = cur_flow == 1 ? 0 : 1;
+                    if (cur_flow == 1) continue;
+                    visit_stack.push_back(visit{cur_node, next_node, cur_dist});
+                }
+            }
+            // assert(old_max_flow < max_flow); //DEBUG
+        }
+    }
+    return max_flow;
 }
 
 
@@ -90,13 +213,13 @@ void solve() {
             i++;
         }
 
-        assert(i == end_node); //DEBUG
+        // assert(i == end_node); //DEBUG
 
         for (const pair<int, int> &pp : start_tiles) {
             int p = pp.first, i = pp.second;
             for (const int &b : get_possible_pos(p, board_size)) {
                 if (board_tiles[b]) continue;
-                assert(end_tiles.count(b)); //DEBUG
+                // assert(end_tiles.count(b)); //DEBUG
                 int j = end_tiles[b];
                 edge *tmp_edge = new edge{0, j};
                 edges[i][j] = tmp_edge;
@@ -106,6 +229,7 @@ void solve() {
     }
 
     // //DEBUG-start
+    // cout << "GRAPH itself:" << endl;
     // for (int i = 0; i < edges.size(); i++) {
     //     for (auto p : edges[i]) {
     //         if (i == p.second->goal) continue;
@@ -115,86 +239,13 @@ void solve() {
     // cout << '\n';
     // //DEBUG-end
 
-    int max_flow = 0;
-    forever {
-        int end_dist = -1; 
-        unordered_set<int> visited;
-        {   //BFS
-            queue<pair<int, int>> visit_queue;
-            visit_queue.push({start_node, 0}); 
-            while (!visit_queue.empty()) {
-                pair<int, int> pp = visit_queue.front();
-                visit_queue.pop();
-                int cur_node = pp.first, cur_dist = pp.second;
-                if (visited.count(cur_node)) continue;
-                visited.insert(cur_node);
-                if (cur_node == end_node) {
-                    end_dist = cur_dist;
-                    break;
-                }
-                cur_dist++;
-                for (const pair<int, edge *> &pp : edges[cur_node]) {
-                    int next_node = pp.first;
-                    if (visited.count(next_node)) continue;
-                    edge *cur_edge = pp.second;
-                    int cur_flow = cur_edge->flow;
-                    if (cur_edge->goal == cur_node) cur_flow = cur_flow == 1 ? 0 : 1;
-                    if (cur_flow == 1) continue;
-                    visit_queue.push({next_node, cur_dist});
-                }
-            }
-        }
-        cout << endl << "end dist = " << end_dist << endl; //DEBUG
-        int old_max_flow = max_flow; //DEBUG
-        if (end_dist == -1) break;
-        visited.clear();
-        {   //DFS
-            vector<visit> visit_stack;
-            visit_stack.push_back(visit{start_node, start_node, 0});
-            vector<int> parents(nodes_amount, -1);
-            while (!visit_stack.empty()) {
-                visit vv = visit_stack.back();
-                visit_stack.pop_back();
-                int 
-                cur_node = vv.current, 
-                par_node = vv.parent,
-                cur_dist = vv.dist;
-                if (end_dist == 9) assert(cur_node != end_node); //DEBUG
-                if (visited.count(cur_node)) continue;
-                parents[cur_node] = par_node;
-                visited.insert(cur_node);
-                if (cur_node == end_node) {
-                    for (; cur_node != start_node; cur_node = par_node, par_node = parents[par_node]) {
-                        edge *cur_edge = edges[cur_node][par_node];
-                        int cur_flow = cur_edge->flow;
-                        if (cur_edge->goal == cur_node) cur_flow = cur_flow == 1 ? 0 : 1;
-                        cur_edge->flow = cur_flow; 
-                    }
-                    max_flow++;
-                    visit_stack.clear();
-                    visit_stack.push_back({start_node, 0});
-                    visited.erase(start_node);
-                    visited.erase(end_node);
-                    continue;
-                } else if (cur_dist == end_dist) continue;
-                cur_dist++;
-                for (const pair<int, edge*> &pp : edges[cur_node]) {
-                    if (visited.count(pp.first)) continue;
-                    int next_node = pp.first;
-                    if (visited.count(next_node)) continue;
-                    edge *cur_edge = pp.second;
-                    int cur_flow = cur_edge->flow;
-                    if (cur_edge->goal == cur_node) cur_flow = cur_flow == 1 ? 0 : 1;
-                    if (cur_flow == 1) continue;
-                    if (cur_dist == end_dist && next_node != end_node) continue;
-                    visit_stack.push_back({cur_node, next_node, cur_dist});
-                }
-            }
-            if (old_max_flow == max_flow) break; //DEBUG
-        }
-    }
-    cout << max_flow << ' ' << tiles_amount << endl << endl; //DEBUG
-    cout << tiles_amount - max_flow;
+    int max_flow = fast_method(edges, nodes_amount, start_node, end_node);
+    // //DEBUG-start
+    // int slow_flow = slow_method(edges, nodes_amount, start_node, end_node);
+    // cout << max_flow << ' ' << slow_flow << endl;
+    // assert(max_flow == slow_flow);
+    // //DEBUG-end
+    cout << tiles_amount - max_flow << endl;
 }
 
 
